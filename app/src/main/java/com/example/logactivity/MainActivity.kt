682c47845e5faf6abc8ae479964a97938da94026
davidyh3 <yuhan59rz@gmail.com>
2024-02-15 02:25:42 -0500

    Add three views, button, editText, and textView to the app.

--- app/src/main/java/com/example/logactivity/MainActivity.kt
+++ app/src/main/java/com/example/logactivity/MainActivity.kt
@@ -3,15 +3,31 @@ package com.example.logactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    private lateinit var editText: EditText
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate")
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            editText.setText("Hello")
+            textView.text = "Hello"
+        }
     }
 
     override fun onStart() {
@@ -48,4 +64,6 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         Log.d(TAG, "onSaveInstanceState")
     }
+
+
 }
